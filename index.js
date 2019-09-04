7df05347a9d9ef82cb646f8bdbf6d4f2bb06e012
kishore k <kishore.k@SW-LP01959.local>
2019-09-04 19:44:58 +0530

    new change conflict

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var PORT = process.env.PORT || 80;
 // App setup
 var app = express();
 var server = app.listen(PORT, function(){
-    console.log('listening for requests on port 4000');
-});
+    
 
 // Static files
 app.use(express.static('public'));
